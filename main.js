39258f1825fe4d273a4a27a414de84e8205306f3
kurang turu <95448883+VERDIBOTZ1@users.noreply.github.com>
2022-05-17 02:23:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ global.DATABASE = global.db // Backwards Compatibility
 
 global.conn = new WAConnection()
 conn.version =[2,2143,3]
-conn.browserDescription =['Figlalox', 'Unavailable', '3.0']
+conn.browserDescription =xnxx.com', 'Unavailible', '3.0']
 let authFile = `${opts._[0] || 'OneBotZ'}.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
